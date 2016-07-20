2e88abda69bd485ea08e576a9964e03436e343d7
andy <andrew.gonzal@gmail.com>
2016-07-19 19:55:58 -0700

    I can call the steam api's now!! Added code for JSON request and extracting the list of games in to a JSONArray.

--- app/src/main/java/lab/oss/andrew/chooseandperish/MainActivity.java
+++ app/src/main/java/lab/oss/andrew/chooseandperish/MainActivity.java
@@ -2,6 +2,17 @@ package lab.oss.andrew.chooseandperish;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textDisplay = (TextView) findViewById(R.id.txtDisplay);
+        String url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=removed&steamid=76561197992593866&include_appinfo=1&format=json";
+
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject object = (JSONObject) new JSONTokener(response.toString()).nextValue();
+                            String steamResponse = object.getString("response");
+                            object = (JSONObject) new JSONTokener(steamResponse).nextValue();
+                            String gameCount = object.getString("game_count");
+                            JSONArray gamesArray = object.getJSONArray("games");
+                            textDisplay.setText(gameCount);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Something helpful, maybe display the error
+                    }
+                });
+
+        RequestSingleton.getInstance(this).addToRequestQueue(jsObjRequest);
     }
 }

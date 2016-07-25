cd83e75f7f737e8f03d78c7f12a2b79e756f3cb1
andy <andrew.gonzal@gmail.com>
2016-07-24 22:42:47 -0700

    After much frustration I finally worked out the work flow. Not using AsyncTask any more.

--- app/src/main/java/lab/oss/andrew/chooseandperish/MainActivity.java
+++ app/src/main/java/lab/oss/andrew/chooseandperish/MainActivity.java
@@ -1,30 +1,25 @@
 package lab.oss.andrew.chooseandperish;
 
-import android.content.Context;
-import android.os.AsyncTask;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.RequestFuture;
-
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private JSONObject jsonResponse = new JSONObject();
+    private JSONObject mJsonResponse = new JSONObject();
+    private JSONArray mJsonArray = new JSONArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,57 +31,55 @@ public class MainActivity extends AppCompatActivity {
 
     public void startWork(View view) {
         ArrayList<String> playerUrls = new ArrayList<String>();
-        EditText urlEdit = (EditText) findViewById(R.id.playerOne);
+        EditText urlEdit;
+
+        for (int i = 0; i < 4; i++) {
+            RelativeLayout layout = (RelativeLayout) findViewById(R.id.mainLayout);
+            View v = layout.getChildAt(i);
+            if (v instanceof EditText) {
+                urlEdit = (EditText) v;
+                if (urlEdit.getText().toString().equals("")) {
+                    playerUrls.add(urlEdit.getText().toString());
+                }
+            }
+        }
+
+        //String steamid = resolveUrl(playerUrls);
 
     }
 
     public void resolveUrl(View view) {
-        EditText urlEdit = (EditText) findViewById(R.id.vanityUrlEdit);
+        EditText urlEdit = (EditText) findViewById(R.id.playerOne);
         String vanity = urlEdit.getText().toString();
         String url = "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/"
                 + "?key=removed"
                 + "&vanityurl=" + vanity
                 + "&url_type=1";
 
-        /*getSteamResponse(url, new VolleyCallback() {
+        getSteamResponse(url, new VolleyCallback() {
             @Override
             public void onSuccess(JSONObject result) {
                 String steamID = result.optString("steamid");
-                mTextDisplay.setText(steamID);
+                getOwnedGames(steamID);
             }
-        });*/
-        new getResponseTask().execute(url);
+        });
 
     }
 
-    /*public void getOwnedGames() {
-        final TextView mTextDisplay = (TextView) findViewById(R.id.txtDisplay);
-        JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONObject object = (JSONObject) new JSONTokener(response.toString()).nextValue();
-                            String steamResponse = object.getString("response");
-                            object = (JSONObject) new JSONTokener(steamResponse).nextValue();
-                            String gameCount = object.getString("game_count");
-                            JSONArray gamesArray = object.getJSONArray("games");
-                            JSONObject game = gamesArray.getJSONObject(0);
-                            int appid = game.getInt("appid");
-                            mTextDisplay.setText(gameCount);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // Something helpful, maybe display the error
-                    }
-                });
+    public void getOwnedGames(String steamID) {
+        String url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"
+                + "?key=removed"
+                + "&steamid=" + steamID
+                + "&include_appinfo=1"
+                + "&format=json";
 
-        VolleySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
-    }*/
+        getSteamResponse(url, new VolleyCallback() {
+            @Override
+            public void onSuccess(JSONObject result) {
+                mJsonArray = result.optJSONArray("games");
+            }
+        });
+    }
 
     private void getSteamResponse(String url, final VolleyCallback callback) {
 
@@ -95,58 +88,28 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            JSONObject object = response.getJSONObject("response");
-                            callback.onSuccess(object);
+                            callback.onSuccess(response.getJSONObject("response"));
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
                 }, new Response.ErrorListener() {
                     @Override
-                    public void onErrorResponse(VolleyError error) {
+                    public void onErrorResponse(VolleyError e) {
                         // Do something
+                        e.printStackTrace();
                     }
                 });
         VolleySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
 
     }
 
-    private void setJsonResponse(JSONObject object) {
-        this.jsonResponse = object;
+    private void setmJsonResponse(JSONObject object) {
+        this.mJsonResponse = object;
     }
 
     public interface VolleyCallback {
         void onSuccess(JSONObject result);
-    }
-
-    private class getResponseTask extends AsyncTask<String, Integer, JSONObject> {
-        Context context = MainActivity.this;
-        final TextView mTextDisplay = (TextView) findViewById(R.id.txtDisplay);
-
-        protected JSONObject doInBackground(String... urls) {
-            RequestFuture<JSONObject> future = RequestFuture.newFuture();
-            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, urls[0], null, future, future);
-            VolleySingleton.getInstance(context).addToRequestQueue(request);
-            JSONObject response = new JSONObject();
-
-            try {
-                response = future.get(10, TimeUnit.SECONDS);
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (TimeoutException e) {
-                e.printStackTrace();
-            }
-
-            return response.optJSONObject("response");
-        }
-
-        protected void onPostExecute(JSONObject result) {
-            String steamID = result.optString("steamid");
-            mTextDisplay.setText(steamID);
-        }
     }
 
 }

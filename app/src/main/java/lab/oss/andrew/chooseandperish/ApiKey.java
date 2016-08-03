cbaf1428bdfc902f40432027e8fc9a7b924f1a43
andy <andrew.gonzal@gmail.com>
2016-08-03 13:11:01 -0700

    added a new class
    -Added ApiKey.java for storing/hiding my api key

---
+++ app/src/main/java/lab/oss/andrew/chooseandperish/ApiKey.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2016 Andrew Gonzalez
+ * This code is available under the "MIT License".
+ * Please see the file COPYING in this distribution for license terms.
+ */
+
+package lab.oss.andrew.chooseandperish;
+
+
+public final class ApiKey {
+    public static final String API_KEY = "";
+
+    private ApiKey() {
+
+    }
+}
